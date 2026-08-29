51950962826de33549570189b46b09f35549fa0a
Christopher J. Seeger <cjseeger@users.noreply.github.com>
2026-08-29 08:45:37 -0500

    Add header comments to main.js
    Added header comments to describe the main application controller for Lyric Solitaire.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-main.js placeholder
+/*
+ * Lyric Solitaire
+ * Main Application Controller
+ *
+ * This file will initialize the game and coordinate
+ * the various game components.
+ */
+
+console.log("Lyric Solitaire main.js loaded.");
